4603d80401bfece41e1e2fff1111742c8a2886e7
Richard Liang <rliang@cfenet.ubc.ca>
2025-05-20 17:19:02 -0700

    hla_algorithm_adaptor.py is working on two test datasets.

--- src/scripts/hla_algorithm_adaptor.py
+++ src/scripts/hla_algorithm_adaptor.py
@@ -7,23 +7,25 @@ from typing import Final, Optional
 
 from pydantic import BaseModel, Field
 
+from easyhla.__about__ import __version__
 from easyhla.easyhla import EasyHLA
 from easyhla.models import (
     AllelePairs,
     HLACombinedStandard,
-    HLAMatchDetails,
     HLAInterpretation,
+    HLAMatchDetails,
+    HLAProteinPair,
     HLASequence,
+    HLAStandard,
 )
 from easyhla.utils import (
     HLA_LOCUS,
+    BadLengthException,
+    InvalidBaseException,
     check_bases,
     check_length,
     nuc2bin,
-    BadLengthException,
-    InvalidBaseException,
 )
-
 
 # These are the "configuration files" that the algorithm uses; these are or may
 # be updated, in which case you specify the path to the new version in the
@@ -103,7 +105,7 @@ class HLAResult(BaseModel):
     ambiguous: bool = False
     homozygous: bool = False
     type: HLA_LOCUS = "B"
-    alg_version: str = "foo"
+    alg_version: str = __version__
     b5701: bool = False
     dist_b5701: int = 0
     errors: list[str] = Field(default_factory=list)
@@ -128,40 +130,47 @@ class HLAResult(BaseModel):
             seqs.append(hla_seq.exon2_str)
             seqs.append(hla_seq.exon3_str)
 
-        # FIXME consider if we should change this format, and unify it with
-        # the clinical driver
         return HLAResult(
             seqs=seqs,
-            alleles_all=aps.stringify(),
+            alleles_all=[f"{x[0]} - {x[1]}" for x in aps.allele_pairs],
             alleles_clean=alleles_clean,
             allele_for_mismatches=f"{rep_ap[0]} - {rep_ap[1]}",
             mismatches=[str(x) for x in match_details.mismatches],
             ambiguous=aps.is_ambiguous(),
             homozygous=aps.is_homozygous(),
             type=interp.locus,
-            alg_version="FOO FIXME",
             b5701=interp.is_b5701(),
             dist_b5701=interp.distance_from_b7501(),
-            errors=[],
         )
-
 
 
 def main():
     hla_input_str: str = ""
     with fileinput.input() as f:
-        hla_input_str = f.read()
+        for line in f:
+            hla_input_str += f"{line}\n"
 
-    hla_input: HLAInput = HLAInput(json.loads(hla_input_str))
+    hla_input: HLAInput = HLAInput(**json.loads(hla_input_str))
 
     errors: list[str] = hla_input.check_sequences()
     if len(errors) > 0:
         error_result: HLAResult = HLAResult(errors=errors)
         print(error_result.model_dump_json())
     else:
-        interp: HLAInterpretation = EasyHLA.interpret(
-            hla_input.hla_sequence()
+        curr_standards: Optional[dict[str, HLAStandard]] = None
+        curr_frequencies: Optional[dict[HLAProteinPair, int]] = None
+        if HLA_FREQUENCIES is not None:
+            with open(HLA_FREQUENCIES) as f:
+                curr_frequencies = EasyHLA.read_hla_frequencies(hla_input.locus, f)
+        if HLA_STANDARDS[hla_input.locus] is not None:
+            with open(HLA_STANDARDS[hla_input.locus]) as f:
+                curr_standards = EasyHLA.read_hla_standards(f)
+        easyhla: EasyHLA = EasyHLA(
+            locus=hla_input.locus,
+            hla_standards=curr_standards,
+            hla_frequencies=curr_frequencies,
         )
+        interp: HLAInterpretation = easyhla.interpret(hla_input.hla_sequence())
         print(HLAResult.build_from_interpretation(interp).model_dump_json())
 
 
