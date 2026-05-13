237676e2d77940c3de3be3776ccd6b0c693dee43
Richard Liang <rliang@cfenet.ubc.ca>
2026-05-13 16:16:49 -0700

    Fixed the "exception" output of interpret_from_json.

--- src/hla_algorithm/interpret_from_json.py
+++ src/hla_algorithm/interpret_from_json.py
@@ -49,7 +49,9 @@ def main():
                 hla_input.hla_sequence(), hla_input.threshold
             )
         except HLAAlgorithm.NoMatchingStandards:
-            print({"exception": "no matching standards"})
+            print(json.dumps({"exception": "no matching standards"}))
+            return
+
         print(
             HLAResult.build_from_interpretation(
                 interp, hla_alg.tag, hla_alg.last_updated
